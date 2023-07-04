67949d6225a193c5421876ec6beb369c7ea48d68
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-04 10:17:49 +0530

    Create 16. SubstringCompare.java

---
+++ Java/16. SubstringCompare.java
@@ -0,0 +1,49 @@
+/* Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings */
+
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        smallest = largest = s.substring(0, k);
+
+    for (int i=1; i<s.length()-k+1; i++) {
+        String substr = s.substring(i, i+k);
+        if (smallest.compareTo(substr) > 0)
+            smallest = substr;
+        if (largest.compareTo(substr) < 0)
+            largest = substr;
+    }
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
